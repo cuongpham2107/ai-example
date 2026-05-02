77805c8b74df11253ad2a6adda4500336244c113
Cuong <cuongpham2107@gmail.com>
2026-05-02 14:54:39 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
-Route::post('/chat/send', [ChatController::class, 'send'])->name('chat.send');
+Route::get('/', [ChatController::class, 'index'])->name('chat.index');
+Route::post('/send', [ChatController::class, 'send'])->name('chat.send');
